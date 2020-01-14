e971c10fb0e990f346154ef7185d7cf886f648d0
klarapacalova <klara.pacalova@st.com>
2020-01-14 14:54:17 +0100

    Update gpio.c

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -72,6 +72,19 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(INT1_master_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = INT2_master_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(INT2_master_GPIO_Port, &GPIO_InitStruct);
+  
+//  /*Configure GPIO pin : PC1 */
+//  GPIO_InitStruct.Pin = GPIO_PIN_1;
+//  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+//  GPIO_InitStruct.Pull = GPIO_NOPULL;
+//  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+//  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+//  
   /*Configure GPIO pins : PAPin PAPin PAPin */
   GPIO_InitStruct.Pin = LD2_Pin|USER3_Pin|USER4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
